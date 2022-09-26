5bf6ca8ae984f452f56ddd2e99ac1dd511bc3357
Andrew-McCall <amccall@qa.com>
2022-09-26 14:11:30 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
+import Header from './components/Header/header';
 import Element from "./components/HelloWorld/helloWorld.jsx"
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Element/>
-      </header>
+      <Header/>
+      <Element/>
     </div>
   );
 }
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+
+    return(
+        <header className="App-header">
+            <nav>
+                <a href="https://www.qa.com" target="_blank" rel="noopener noreferrer"> Qa Website </a>
+                <a href="/"> Home </a>
+            </nav>
+        </header>
+    )
+    
+}
+
+export default Header;
