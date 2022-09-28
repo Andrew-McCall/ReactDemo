e317eeb79673104e45848e6eb2822ac6ce1014d4
Andrew-McCall <amccall@qa.com>
2022-09-28 15:14:36 +0100

    APIs

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,18 +1,33 @@
 import Message from "./message";
-
+import axios from "axios"
 import {useParams} from "react-router-dom"
+import {useState, useEffect} from "react"
 
 const TestPage = () => {
 
     const {id} = useParams();
 
+    const [fact, setFact] = useState("Loading!");
+
+    useEffect( () => {
+        // fetch("https://catfact.ninja/fact").then(res => {
+        //     res.json().then(data => {
+        //         setFact(data.fact)
+        //     })
+        // })
+        axios.get("https://catfact.ninja/fact").then(res => {
+            setFact(res.data.fact)
+        })
+    }, [])
+
     return(
-    <div>
+        <div>
 
-        <h1>Test Page</h1>
-        <Message isGreeting={(id%2 === 0)} name={id}/>
+            <h1>Test Page</h1>
+            <Message isGreeting={(id%2 === 0)} name={id}/>
+            <h3>{fact}</h3>
 
-    </div>
+        </div>
     )
 }
 
